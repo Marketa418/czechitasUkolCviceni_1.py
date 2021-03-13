378c3dc7f44a8eaf967a90bc635bcc9c312eca96
Marketa418 <dlabolova3@seznam.cz>
2021-03-13 23:23:37 +0100

    ukol z lekce 2 číslo 10 - opraveno :)

--- ukol10.py
+++ ukol10.py
@@ -31,13 +31,11 @@ zeme = input("V jaké zemi podnikáte: ")
 konference = input("Pořádáte konference? Pokud ne, tak nevyplňujte kolonku. ")
 newsletter = input("Posíláte newslettery? Pokud ne, tak nevyplňujte kolonku.")
 
-point = total_points(odvetvi, obrat, zeme, konference = False, newsletter = False)
+point = total_points(odvetvi, obrat, zeme, konference, newsletter)
 if point <= 5:
   print ("Malá šance na úspěch.")
-elif point > 6:
-  print ("Střední šance na úspěch.")
-elif point <= 8:
-  print ("Střední šance na úspěch.")
+elif point < 8:
+  print("Střední šance na úspěch.")
 else:
   print("Šance na úspěch je vysoká.")
 print (point)
