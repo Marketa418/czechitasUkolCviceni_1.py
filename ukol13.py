3cb40b9f7720d162fa0f282a3931f86d7b655648
Marketa418 <dlabolova3@seznam.cz>
2021-03-13 23:26:53 +0100

    ukol z lekce 3 - 13 a 15 neopraveno, nevím jak :(

--- ukol13.py
+++ ukol13.py
@@ -20,30 +20,30 @@ class Cars:
         else:
             print("Automobil je půjčený.")
 
-    #def vrat_auto(self, km, pocet_pujcenych_dni):
-            #   if self.pocet_najetych_km <= km:
-            #self.pocet_najetych_km += km
-        #celkova_cena = pocet_pujcenych_dni
-            # if pocet_pujcenych_dni < 7:
-            #celkova_cena = pocet_pujcenych_dni * 400
-            #print(f"Cena za půjčení auta je {celkova_cena} Kč za {pocet_pujcenych_dni} dní.")
-            #else:
-            #   celkova_cena = pocet_pujcenych_dni * 300
-    #      print(f"Cena za půjčení auta je {celkova_cena} Kč za {pocet_pujcenych_dni} dní.")
+    def vrat_auto(self, km, pocet_pujcenych_dni):
+        if self.pocet_najetych_km <= km:
+            self.pocet_najetych_km += km
+        if pocet_pujcenych_dni < 7:
+            celkova_cena = pocet_pujcenych_dni * 400
+            return f"Cena za půjčení auta je {celkova_cena} Kč za {pocet_pujcenych_dni} dní."
+        else:
+            celkova_cena = pocet_pujcenych_dni * 300
+            return f"Cena za půjčení auta je {celkova_cena} Kč za {pocet_pujcenych_dni} dní."
 
 peugeot = Cars("4A2 3020", "Peugeot 403 Cabrio", "47534")
 octavia = Cars ("1P3 4747", "Škoda Octavia", "41253")
 
 pocet_pujcenych_dni = int(input("Kolik dní jste měl auto půjčené? "))
 pocet_najetych_km = input("Kolik km jste s autem najel? ")
+print(pocet_pujcenych_dni.vrat_auto())
 
-celkova_cena = pocet_pujcenych_dni
-if pocet_pujcenych_dni < 7:
-    celkova_cena = pocet_pujcenych_dni * 400
-    print(f"Cena za půjčení auta je {celkova_cena} Kč za {pocet_pujcenych_dni} dní.")
-else:
-    celkova_cena = pocet_pujcenych_dni * 300
-    print(f"Cena za půjčení auta je {celkova_cena} Kč za {pocet_pujcenych_dni} dní.")
+#celkova_cena = pocet_pujcenych_dni
+#if pocet_pujcenych_dni < 7:
+#   celkova_cena = pocet_pujcenych_dni * 400
+#   print(f"Cena za půjčení auta je {celkova_cena} Kč za {pocet_pujcenych_dni} dní.")
+#else:
+#   celkova_cena = pocet_pujcenych_dni * 300
+#   print(f"Cena za půjčení auta je {celkova_cena} Kč za {pocet_pujcenych_dni} dní.")
 
 print(peugeot.getInfo())
 print(octavia.getInfo())
--- ukol15.py
+++ ukol15.py
@@ -2,18 +2,18 @@ from datetime import datetime, timedelta
 
 datum = input("Uveďte datum, kdy chcete jít do kina: ")
 pocet_osob = int(input("Pro kolik osob chcete lístky: "))
-datum = datetime.strftime("%Y. %m. %d.")
+datum_novy = datum.strftime("%Y. %m. %d.")
 
 start_hlavni_sezona = datetime(2021, 7, 1)
 konec_hlavni_seozna = datetime(2021, 8, 10)
 start_vedlejsi_sezona = datetime(2021, 8, 11)
 konec_vedlejsi_seozna = datetime(2021, 8, 31)
 
-if datum == start_hlavni_sezona and konec_hlavni_seozna:
-  cena_za_listy = pocet_osob * 250
+if datum_novy == start_hlavni_sezona and konec_hlavni_seozna:
+  cena_za_listky = pocet_osob * 250
   print(f"Cena za vstupenky je {cena_za_listky}.")
-elif datum == start_vedlejsi_sezona and konec_vedlejsi_seozna:
-  cena_za_listy = pocet_osob * 180
+elif datum_novy == start_vedlejsi_sezona and konec_vedlejsi_seozna:
+  cena_za_listky = pocet_osob * 180
   print(f"Cena za vstupenky je {cena_za_listky}.")
 else:
   print("Kino není otevřené.")
