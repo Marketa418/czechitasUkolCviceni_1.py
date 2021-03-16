4179600d86a70c5c6f85793b41da95b57cd367f4
Marketa418 <dlabolova3@seznam.cz>
2021-03-16 10:41:53 +0100

    ukol z lekce 4 - příklad 18

---
+++ ukol18.py
@@ -0,0 +1,30 @@
+from datetime import datetime, timedelta
+
+class Kontakt:
+  def __init__(self, jmeno, email):
+    self.jmeno = jmeno
+    self.email = email
+
+class Uchazec (Kontakt):
+    def __init__(self, jmeno, email, datum_pohovoru, zapis_z_pohovoru = ""):
+        super().__init__(jmeno, email)
+        self.datum_pohovoru = datum_pohovoru
+        self.zapis_z_pohovoru = zapis_z_pohovoru
+
+    def pridej_zapis(self):
+        self.datum_pohovoru = datetime.strptime(self.datum_pohovoru,"%d. %m. %Y")
+        if datetime.now() < self.datum_pohovoru:
+            print("Pohovor ještě neproběhl.")
+        else:
+            #self.zapis_z_pohovoru += zapis_z_pohovoru
+            print(f"Zápis byl uložen. Zápis z pohovoru je {self.zapis_z_pohovoru}")
+
+    def get_Info(self, zapis_z_pohovoru):
+        return f"Jméno nového uchazeče je {self.jmeno}, jeho email je {self.email} na pohovor přišel dne {self.datum_pohovoru}. Zápis z pohovoru je: {zapis_z_pohovoru}."
+
+pepa = Uchazec("Pepa urbanec", "pepa.urbanec.seznam.cz", "26. 2. 2021", "Pepa byl dokonalý uchazeč.")
+martin = Uchazec("Martin Straka", "martin.straka.gmail.com", "5. 4. 2021", "")
+alena = Uchazec("Alena Mánesová", "alenka.z.rise.divu.com", "3. 3. 2021", "Umí komunikovat s lidmi.")
+print(pepa.pridej_zapis())
+print(martin.pridej_zapis())
+print(alena.pridej_zapis())
