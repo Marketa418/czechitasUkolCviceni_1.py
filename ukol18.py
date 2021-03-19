4c24a806de6506bcec25cf6b8c52629b97118ce7
Marketa418 <dlabolova3@seznam.cz>
2021-03-20 00:38:10 +0100

    ukol z lekce 4 - příklad 18 opraveno

--- ukol18.py
+++ ukol18.py
@@ -11,20 +11,20 @@ class Uchazec (Kontakt):
         self.datum_pohovoru = datum_pohovoru
         self.zapis_z_pohovoru = zapis_z_pohovoru
 
-    def pridej_zapis(self):
+    def pridej_zapis(self, zapis_z_pohovoru):
         self.datum_pohovoru = datetime.strptime(self.datum_pohovoru,"%d. %m. %Y")
         if datetime.now() < self.datum_pohovoru:
             print("Pohovor ještě neproběhl.")
         else:
-            #self.zapis_z_pohovoru += zapis_z_pohovoru
+            self.zapis_z_pohovoru += zapis_z_pohovoru
             print(f"Zápis byl uložen. Zápis z pohovoru je {self.zapis_z_pohovoru}")
 
-    def get_Info(self, zapis_z_pohovoru):
+    def get_Info(self):
         return f"Jméno nového uchazeče je {self.jmeno}, jeho email je {self.email} na pohovor přišel dne {self.datum_pohovoru}. Zápis z pohovoru je: {zapis_z_pohovoru}."
 
-pepa = Uchazec("Pepa urbanec", "pepa.urbanec.seznam.cz", "26. 2. 2021", "Pepa byl dokonalý uchazeč.")
-martin = Uchazec("Martin Straka", "martin.straka.gmail.com", "5. 4. 2021", "")
-alena = Uchazec("Alena Mánesová", "alenka.z.rise.divu.com", "3. 3. 2021", "Umí komunikovat s lidmi.")
-print(pepa.pridej_zapis())
-print(martin.pridej_zapis())
-print(alena.pridej_zapis())
+pepa = Uchazec("Pepa urbanec", "pepa.urbanec.seznam.cz", "26. 2. 2021")
+martin = Uchazec("Martin Straka", "martin.straka.gmail.com", "5. 4. 2021")
+alena = Uchazec("Alena Mánesová", "alenka.z.rise.divu.com", "3. 3. 2021")
+print(pepa.pridej_zapis("Pepa byl dokonalý uchazeč."))
+print(martin.pridej_zapis(""))
+print(alena.pridej_zapis("Umí komunikovat s lidmi."))
